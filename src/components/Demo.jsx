3bc21ddf25b6926d0b31fd3a9ab6e12a88545e4b
roopeky <roope.kylli@metropolia.fi>
2023-10-26 19:04:35 +0300

    demo comp progress and few tweaks

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,5 +1,44 @@
+import { useState, useEffect } from "react";
+import { copy, link, loader, tick } from "../assets";
+
+const handleSubmit = () => {};
+
+const handleChange = () => {};
+
 const Demo = () => {
-  return <div>demo</div>;
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      {/*search here*/}
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={link}
+            alt="link icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value=""
+            onChange={handleChange}
+            required
+            className="url_input peer"
+          />
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            Enter
+          </button>
+        </form>
+        {/*browse url history here*/}
+      </div>
+      {/*display results here*/}
+    </section>
+  );
 };
 
 export default Demo;
