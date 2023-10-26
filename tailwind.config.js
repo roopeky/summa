d23b9252f5f2b9927003d57ac50c0e0d5b0d51af
roopeky <roope.kylli@metropolia.fi>
2023-10-26 18:07:36 +0300

    configs ready

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./src/**/*.{html,js}"];
+export const theme = {
+  extend: {
+    fontFamily: {
+      satoshi: ["Satoshi", "sans-serif"],
+      inter: ["Inter", "sans-serif"],
+    },
+  },
+};
+export const plugins = [];
